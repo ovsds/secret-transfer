198bee160371ed7a18ab37d153cf54111958f770
ovsds <ovsds@yandex-team.ru>
2024-02-20 23:38:12 +0100

    docs: add README for custom class definitions

--- tests/unit/core/destination/test_base.py
+++ tests/unit/core/destination/test_base.py
@@ -3,16 +3,3 @@ import secret_transfer.core as core
 
 def test_abstract_class_not_registered():
     assert core.AbstractDestination.__name__ not in core.DestinationRegistry.classes
-
-
-def test_register():
-    class TestDestination(core.AbstractDestination):
-        __register__ = False
-
-    name = "test_name"
-
-    test_source = TestDestination()
-    test_source.register(name)
-
-    assert name in core.DestinationRegistry.instances
-    assert test_source is core.DestinationRegistry.instances[name]
