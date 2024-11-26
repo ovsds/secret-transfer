028d5777c7331a58a89e2b241e9d975edaf48c26
ovsds <ovsds@yandex-team.ru>
2024-11-26 17:57:37 +0100

    ci: fix lint

--- secret_transfer/core/base/registry.py
+++ secret_transfer/core/base/registry.py
@@ -7,7 +7,7 @@ class RegistrationMeta(type):
     def __init__(cls, name: str, bases: tuple[type, ...], attrs: dict[str, typing.Any]):
         cls.classes: dict[str, type] = {}
         cls.instances: dict[str, typing.Any] = {}
-        cls.default_class: typing.Optional[type] = None
+        cls.default_class: typing.Optional[typing.Any] = None
 
 
 class BaseRegistry(type, typing.Generic[protocol.ResourceType], metaclass=RegistrationMeta):
@@ -24,12 +24,13 @@ class BaseRegistry(type, typing.Generic[protocol.ResourceType], metaclass=Regist
     default_class: typing.Optional[type[protocol.ResourceType]]
 
     def __init__(
-        cls: type[protocol.ResourceType],
+        cls,
         name: str,
         bases: tuple[type, ...],
         attrs: dict[str, typing.Any],
     ):
         super().__init__(cls)
+        cls = typing.cast(type[protocol.ResourceType], cls)
 
         if "__register__" not in attrs or attrs["__register__"]:
             cls.register_class(name, cls)  # pyright: ignore[reportAttributeAccessIssue]
